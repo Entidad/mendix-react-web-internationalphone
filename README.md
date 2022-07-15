477ca53b9cc6d412d8611c0c83928ae06fbd69eb
skullquake <ockert8080@gmail.com>
2022-07-15 22:13:15 +0200

    .

---
+++ README.md
@@ -0,0 +1,31 @@
+## Mendix React Web DatePicker
+Mendix widget implementing [https://github.com/jackocnr/intl-tel-input#events](https://github.com/jackocnr/intl-tel-input#events)
+
+## Features
+International phone features
+
+## Usage
+Place widget in data view and configure attributes.
+
+## Demo project
+`./test/`
+
+
+## Issues, suggestions and feature requests
+[GitHub](https://github.com/Entidad/mendix-react-web-internationalphone/issues)
+
+## Development and contribution
+
+1. Install NPM package dependencies by using: `npm install`. If you use NPM v7.x.x, which can be checked by executing `npm -v`, execute: `npm install --legacy-peer-deps`.
+2. Install further dependencies using `npm install intl-tel-input --save`
+3. Run `npm start` to watch for code changes. On every change:
+    - the widget will be bundled;
+    - the bundle will be included in a `dist` folder in the root directory of the project;
+    - the bundle will be included in the `deployment` and `widgets` folder of the Mendix test project.
+
+Contributions welcome
+
+## References
+
+* [https://github.com/ollyspooner/mx-widget-IntlPhoneInput](https://github.com/ollyspooner/mx-widget-IntlPhoneInput)
+* [https://github.com/jackocnr/intl-tel-input](https://github.com/jackocnr/intl-tel-input)
---
+++ src/InternationalPhone.jsx
@@ -0,0 +1,87 @@
+import{Component,createElement}from"react";
+import{InternationalPhoneComponent}from"./components/InternationalPhoneComponent";
+import"./ui/InternationalPhone.css";
+export default class InternationalPhone extends Component{
+	constructor(props){
+		super(props);
+		window.wmain=this;
+	}
+	onChange(value){
+		if(this.props.value&&this.props.value.status=="available")this.props.value.setValue(value);
+		if(
+			this.props.onChangeAction
+		){
+			if(
+				this.props.onChangeAction.canExecute&&
+				this.props.onChangeAction.isAuthorized&&
+				!(
+					this.props.onChangeAction.isExecuting&&
+					this.props.onChangeAction.disabledDuringExecution
+				)
+			){
+				this.props.onChangeAction.execute();
+			}
+		}
+	}
+	setIso2(value){
+		const initialCountryAttribute=this.props.initialCountryAttribute.value||"";
+		if(this.props.initialCountryAttribute&&this.props.initialCountryAttribute.status=="available")this.props.initialCountryAttribute.setValue(value);
+		if(
+			this.props.onChangeAction
+		){
+			if(
+				this.props.onChangeAction.canExecute&&
+				this.props.onChangeAction.isAuthorized&&
+				!(
+					this.props.onChangeAction.isExecuting&&
+					this.props.onChangeAction.disabledDuringExecution
+				)
+			){
+				this.props.onChangeAction.execute();
+			}
+		}
+	}
+
+	render(){
+		const value=this.props.value.value||"";
+		const validation=this.props.value.validation;
+		//--------------------------------------------------------------------------------
+		const allowDropdown=this.props.allowDropdown||null;
+		const autoHideDialCode=this.props.autoHideDialCode||null;
+		const autoPlaceholder=this.props.autoPlaceholder||"";
+		const excludeCountries=this.props.excludeCountries||[];
+		const formatOnDisplay=this.props.formatOnDisplay||null;
+		const initialCountry=this.props.initialCountry.value||"";
+		const initialCountryAttribute=this.props.initialCountryAttribute.value||"";
+		const localizedCountries=this.props.localizedCountries||[];
+		const nationalMode=this.props.nationalMode||null;
+		const onlyCountries=this.props.onlyCountries||[];
+		const placeholderNumberType=this.props.placeholderNumberType||"";
+		const preferredCountries=this.props.preferredCountries||[];
+		const separateDialCode=this.props.separateDialCode||null;
+		//--------------------------------------------------------------------------------
+		return(
+			<InternationalPhoneComponent
+				onChange={(value)=>{this.onChange(value);}}
+				setIso2={(value)=>{this.setIso2(value);}}
+				value={value}
+				validation={validation}
+
+				allowDropdown={allowDropdown}
+				autoHideDialCode={autoHideDialCode}
+				autoPlaceholder={autoPlaceholder}
+				excludeCountries={excludeCountries}
+				formatOnDisplay={formatOnDisplay}
+				initialCountry={initialCountryAttribute}
+				localizedCountries={localizedCountries}
+				nationalMode={nationalMode}
+				onlyCountries={onlyCountries}
+				placeholderNumberType={placeholderNumberType}
+				preferredCountries={preferredCountries}
+				separateDialCode={separateDialCode}
+
+
+			/>
+		);
+	}
+}
